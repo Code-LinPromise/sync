ada0510c1708f61e9451ad250ea5ba15dd9aa5a2
lin pengtao <326661445@qq.com>
2024-03-11 22:03:17 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os/exec"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	go func() {
+		gin.SetMode(gin.DebugMode)
+		router := gin.Default()
+		router.GET("/", func(c *gin.Context) {
+			c.Writer.Write([]byte("sdsd"))
+		})
+		router.Run(":8080")
+	}()
+	// 先写死路径，后面再照着 lorca 改
+	chromePath := "C:\\Users\\林鹏涛\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe"
+	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:8080/")
+	cmd.Start()
+	select {}
+}
